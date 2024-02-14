e6ffe397be47da2ccf5d7b3d61d608c3ed0b299b
Filip Zawartka <filpz.08@gmail.com>
2024-02-14 21:43:26 +0100

    change OSM map style

--- src/osm.js
+++ src/osm.js
@@ -3,7 +3,7 @@ import "leaflet/dist/leaflet.css";
 
 let map = L.map("map").setView([53.49967738513314, 10.0028454236408], 17);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+L.tileLayer("https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=0ddccd73675a444fb31f35acdb4e0619", {
 	maxZoom: 19,
 	attribution:
 		"&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>",
