b82f26a2187414e9ec021c753f969d13a4b8d53d
Filip Zawartka <filpz.08@gmail.com>
2024-02-16 12:00:55 +0100

    add filter and help btns, add logo

--- src/form.js
+++ src/form.js
@@ -1,8 +1,14 @@
 const searchField = document.getElementById("address-search");
-let timeout = null;
 const suggestionBox = document.getElementById("address-suggestion");
 const form = document.getElementById("form");
 const overlay = document.getElementById("overlay");
+const helpBtn = document.getElementById("help-btn");
+const formBtn = document.getElementById("filter-btn");
+const closeBtn = document.getElementById("close-btn");
+const helpPage = document.getElementById("help-page");
+let timeout = null;
+let helpIsOpen = false;
+let formIsOpen = false;
 
 async function fetchData() {
 	const res = await fetch(
@@ -17,14 +23,12 @@ function fillSuggestionBox(searchRes) {
 	let plz = searchRes[0]?.address?.postcode;
 	let suburb = searchRes[0]?.address?.suburb ?? searchRes[0]?.address?.city_district;
 	let city = searchRes[0]?.address?.city ?? searchRes[0]?.address?.town;
-	
+
 	if (typeof plz == "undefined") {
-		suggestionBox.innerText = "Unbekannte Adresse 😢"; 
-	}
-	else {
+		suggestionBox.innerText = "Unbekannte Adresse 😢";
+	} else {
 		suggestionBox.innerText = `Es wird gesucht in: ${plz} ${suburb}, ${city}`;
 	}
-
 }
 
 searchField.addEventListener("input", () => {
@@ -40,12 +44,31 @@ document.getElementById("resetBtn").addEventListener("click", () => {
 	suggestionBox.innerText = "";
 });
 
-document.getElementById("closeBtn").addEventListener("click", () => {
+closeBtn.addEventListener("click", () => {
+	console.log("closeBtn clicked");
+	formIsOpen = false;
 	form.classList.add("hidden");
 	overlay.classList.add("hidden");
 });
 
-//document.getElementById("openFormBtn").addEventListener("click", () => {
-// 	form.classList.remove("hidden");
-// 	overlay.classList.remove("hidden");
-// });
+formBtn.addEventListener("click", () => {
+	formIsOpen = true;
+	form.classList.toggle("hidden");
+	if (helpIsOpen) {
+		helpIsOpen = false;
+		helpPage.classList.add("hidden");
+	} else {
+		overlay.classList.toggle("hidden");
+	}
+});
+
+helpBtn.addEventListener("click", () => {
+	helpIsOpen = true;
+	helpPage.classList.remove("hidden");
+	if (formIsOpen) {
+		formIsOpen = false;
+		form.classList.add("hidden");
+	} else {
+		overlay.classList.toggle("hidden");
+	}
+});
