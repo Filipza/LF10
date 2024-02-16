f73a62c47eff68ed7ff6b3a8d46cfb7d22d5a67e
Faby <fmlabus@gmail.com>
2024-02-16 15:16:39 +0100

    create table and add migrations folder

---
+++ backend/migrations/1.sql
@@ -0,0 +1,16 @@
+CREATE TYPE LOCATION_TYPE AS ENUM ('Altglas', 'Altkleider', 'Altpapier', 'Elektrokleingeräte', 'Recyclinghof');
+
+CREATE TABLE IF NOT EXISTS locations (
+    id SERIAL PRIMARY KEY,
+    address JSON NOT NULL,
+    coordinates POINT NOT NULL,
+    type LOCATION_TYPE NOT NULL,
+    rating INT,
+    info TEXT
+);
+
+-- INSERT INTO locations (address, coordinates, type) VALUES (
+--     '{"street": "Baumeisterstraße 17","postalcode": 20099,"city": "Hamburg"}',
+--     POINT(53.55512071027223, 10.010559453442404),
+--     'Altglas'
+-- );
