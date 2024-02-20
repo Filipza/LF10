0f9313cc531707a489ca450bcfb94496c6ac7606
Filip Z <filipz.08@gmail.com>
2024-02-20 21:41:15 +0100

    begin .gml parsing

--- xmlToDb/main.go
+++ xmlToDb/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 
 	"github.com/everystreet/go-proj/v8/proj"
 )
 
+type FeatureCollection struct {
+	XMLName       xml.Name        `xml:"FeatureCollection"`
+	FeatureMember []FeatureMember `xml:"featureMember"`
+}
+
+type FeatureMember struct {
+	RecyclingPoint RecyclingPoint `xml:"abfall_recycling_1_papier"`
+}
+
+type RecyclingPoint struct {
+	ID                       string `xml:"id,attr"`
+	Depotnummer              string `xml:"Depotnummer"`
+	Ortsteil                 string `xml:"Ortsteil"`
+	Strasse                  string `xml:"Strasse"`
+	PLZ                      string `xml:"PLZ"`
+	AltpapierCount           int    `xml:"Altpapier-Depotcontainer"`
+	GruenglasCount           int    `xml:"Grünglas-Depotcontainer"`
+	WeissglasCount           int    `xml:"Weissglas-Depotcontainer"`
+	WertstoffCount           int    `xml:"Wertstoff-Depotcontainer"`
+	ElektrokleinGeraeteCount int    `xml:"Elektrokleingeräte-Depotcontainer"`
+	AlttextilCount           int    `xml:"Alttextil-Depotcontainer"`
+}
+
+type Point struct {
+	Pos string `xml:"pos"`
+}
+
 func main() {
-	ToEPSG25832(562609.232, 5935944.481, "EPSG:25832")
 
 }
 
+// ToEPSG25832(562609.232, 5935944.481, "EPSG:25832")
 func ToEPSG25832(x, y float64, projDefinition string) (float64, float64) {
 	coord := proj.XY{X: x, Y: y}
 
