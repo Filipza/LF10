c0f1b0819aaa45b04eaff5f8ab3ef58886b353b4
Filip Zawartka <filpz.08@gmail.com>
2024-02-21 16:17:41 +0100

    added gml parsing and coordinate conversion

--- xmlToDb/main.go
+++ xmlToDb/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/everystreet/go-proj/v8/proj"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type FeatureCollection struct {
@@ -17,17 +21,22 @@ type FeatureMember struct {
 }
 
 type RecyclingPoint struct {
-	ID                       string `xml:"id,attr"`
-	Depotnummer              string `xml:"Depotnummer"`
-	Ortsteil                 string `xml:"Ortsteil"`
-	Strasse                  string `xml:"Strasse"`
-	PLZ                      string `xml:"PLZ"`
-	AltpapierCount           int    `xml:"Altpapier-Depotcontainer"`
-	GruenglasCount           int    `xml:"Grünglas-Depotcontainer"`
-	WeissglasCount           int    `xml:"Weissglas-Depotcontainer"`
-	WertstoffCount           int    `xml:"Wertstoff-Depotcontainer"`
-	ElektrokleinGeraeteCount int    `xml:"Elektrokleingeräte-Depotcontainer"`
-	AlttextilCount           int    `xml:"Alttextil-Depotcontainer"`
+	ID                       string   `xml:"id,attr"`
+	Depotnummer              string   `xml:"Depotnummer"`
+	Ortsteil                 string   `xml:"Ortsteil"`
+	Strasse                  string   `xml:"Strasse"`
+	PLZ                      string   `xml:"PLZ"`
+	AltpapierCount           int      `xml:"Altpapier-Depotcontainer"`
+	GruenglasCount           int      `xml:"Grünglas-Depotcontainer"`
+	WeissglasCount           int      `xml:"Weissglas-Depotcontainer"`
+	WertstoffCount           int      `xml:"Wertstoff-Depotcontainer"`
+	ElektrokleinGeraeteCount int      `xml:"Elektrokleingeräte-Depotcontainer"`
+	AlttextilCount           int      `xml:"Alttextil-Depotcontainer"`
+	Geo                      Geometry `xml:"geom"`
+}
+
+type Geometry struct {
+	Point Point `xml:"Point"`
 }
 
 type Point struct {
@@ -35,26 +44,58 @@ type Point struct {
 }
 
 func main() {
+	// Open your GML file
+	file, err := os.Open("container.gml")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
+	// Decode the XML
+	var fc FeatureCollection
+	decoder := xml.NewDecoder(file)
+	err = decoder.Decode(&fc)
+	if err != nil {
+		panic(err)
+	}
+
+	ToEPSG25832(fc.FeatureMember[0].RecyclingPoint.Geo.Point.Pos, "EPSG:25832")
 }
 
 // ToEPSG25832(562609.232, 5935944.481, "EPSG:25832")
-func ToEPSG25832(x, y float64, projDefinition string) (float64, float64) {
+func ToEPSG25832(coords string, projDefinition string) (*pgtype.Point, error) {
+	coordsSplitted := strings.Split(coords, " ")
+	x, err := strconv.ParseFloat(coordsSplitted[0], 64)
+	if err != nil {
+		fmt.Println("failed to parse x coordinate")
+		return nil, nil
+	}
+
+	y, err := strconv.ParseFloat(coordsSplitted[1], 64)
+	if err != nil {
+		fmt.Println("failed to parse y coordinate")
+		return nil, nil
+	}
+
 	coord := proj.XY{X: x, Y: y}
 
-	err := proj.CRSToCRS(
+	err = proj.CRSToCRS(
 		projDefinition,
 		"EPSG:4326",
 		func(pj proj.Projection) {
 			proj.TransformForward(pj, &coord)
-			// transform more coordinates
 		},
 	)
 
 	if err != nil {
-		return 0, 0
+		fmt.Println("failed to convert to other CRS")
+		return nil, nil
 	}
 
-	fmt.Println(coord.Y, coord.X)
-	return coord.Y, coord.X
+	point := pgtype.Point{
+		P:     pgtype.Vec2{X: x, Y: y},
+		Valid: true,
+	}
+
+	return &point, err
 }
